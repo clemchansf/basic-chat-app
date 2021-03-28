9497567c5178c722d0afca10ff34466d1d7ee663
Clem <clem.chan.sf@gmail.com>
2021-03-28 15:03:46 -0700

    Add jasmine tdd framework and server test spec js

---
+++ spec/server.spec.js
@@ -0,0 +1,24 @@
+var request = require("request")
+
+describe("calc", () => {
+  it("should multiple 2x2", () => {
+    expect(2 * 2).toBe(4)
+  })
+})
+
+describe("get messages", () => {
+  it("should return 200 ok", done => {
+    request.get("http://localhost:3000/messages", (err, response) => {
+      console.log(response.body)
+      expect(response.statusCode == 200).toBe(true)
+      done()
+    })
+  })
+  it("should return a list, that's not empty", done => {
+    request.get("http://localhost:3000/messages", (err, response) => {
+      console.log(response.body)
+      expect(response.body.length).toBeGreaterThan(0)
+      done()
+    })
+  })
+})
