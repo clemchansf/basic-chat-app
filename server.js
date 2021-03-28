8bbede546e1c78423cbbf8f75860232d59cec0bb
Clem <clem.chan.sf@gmail.com>
2021-03-28 13:59:02 -0700

    Save and retreive message from mongodb

--- server.js
+++ server.js
@@ -9,27 +9,41 @@ var io = require("socket.io")(http)
 var mongoose = require("mongoose")
 var dbUrl = process.env.CONNECTIONSTRING
 
-var messages = [
-  { name: "Tim", message: "Hi" },
-  { name: "Jane", message: "Hello" }
-]
+var Message = mongoose.model("message", {
+  name: String,
+  message: String
+})
+
+// var messages = [
+//   { name: "Tim", message: "Hi" },
+//   { name: "Jane", message: "Hello" }
+// ]
 
 app.use(express.static(__dirname))
 app.use(bodyParse.json())
 app.use(bodyParse.urlencoded({ extended: false }))
 
 app.get("/messages", (req, res) => {
-  res.send(messages)
+  Message.find({}, (err, messages) => {
+    res.send(messages)
+  })
 })
 
 app.post("/messages", (req, res) => {
   // console.log(req.body)
   if (!Boolean(req.body.target)) {
-    messages.push(req.body)
-    io.emit("message", req.body)
+    var message = new Message(req.body)
+    message.save(err => {
+      if (err) {
+        res.sendStatus(500)
+      }
+      // messages.push(req.body)
+      io.emit("message", req.body)
+      res.sendStatus(200)
+    })
+  } else {
+    res.sendStatus(404)
   }
-  console.log(messages)
-  res.sendStatus(200)
 })
 
 mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true }, err => {
