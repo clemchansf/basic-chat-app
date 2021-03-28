79a46b96ab18ac6a1429c862ccb6a53e11e91368
Clem <clem.chan.sf@gmail.com>
2021-03-28 12:47:56 -0700

    Fix express bug to filter content-script, add post

--- server.js
+++ server.js
@@ -1,11 +1,28 @@
 var express = require("express")
+var bodyParse = require("body-parser")
 var app = express()
 
-app.use(express.static(__dirname))
+var messages = [
+  { name: "Tim", message: "Hi" },
+  { name: "Jane", message: "Hello" }
+]
 
-// var server = app.listen(3000, () => {
-//   console.log("server is listening on port", server.address().port)
-// })
+app.use(express.static(__dirname))
+app.use(bodyParse.json())
+app.use(bodyParse.urlencoded({ extended: false }))
+
+app.get("/messages", (req, res) => {
+  res.send(messages)
+})
+
+app.post("/messages", (req, res) => {
+  console.log(req.body)
+  if (!Boolean(req.body.target)) {
+    messages.push(req.body)
+  }
+  console.log(messages)
+  res.sendStatus(200)
+})
 
 var server = app.listen(3000, () => {
   console.log("Server is listening on port", server.address().port)
